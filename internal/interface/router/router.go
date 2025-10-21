e3428f9047c4c0ef04dfeb933c7b5f4d61058e94
giroaon2 <giroaon2@gmail.com>
2025-10-21 23:07:43 +0700

    Update router.go

--- internal/interface/router/router.go
+++ internal/interface/router/router.go
@@ -1,1 +1,18 @@
-r.POST("/upload", blobHandler.UploadFile)
+package router
+
+import (
+    "github.com/gin-gonic/gin"
+    "phillip-cms-jobs/internal/interface/handler"
+)
+
+func NewRouter() *gin.Engine {
+    r := gin.Default()
+
+    // สร้าง handler (สามารถ inject usecase ที่ต้องการได้)
+    blobHandler := handler.NewBlobHandler()
+
+    // กำหนด route
+    r.POST("/upload", blobHandler.UploadFile)
+
+    return r
+}

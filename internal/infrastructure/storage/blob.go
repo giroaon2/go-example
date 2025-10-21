48d9657588330f17a9bab156b905f554fea65210
giroaon2 <giroaon2@gmail.com>
2025-10-21 23:04:55 +0700

    Update blob.go

--- internal/infrastructure/storage/blob.go
+++ internal/infrastructure/storage/blob.go
@@ -1,5 +1,32 @@
+package storage
+
+import (
+    "context"
+    "io"
+    "log"
+
+    "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+)
+
+type BlobStorage struct {
+    client        *azblob.ServiceClient
+    containerName string
+}
+
+func NewBlobStorage() *BlobStorage {
+    // ตัวอย่างสร้าง client Azure (สมมติมี endpoint + credential)
+    client, err := azblob.NewServiceClientWithNoCredential("https://<your_account>.blob.core.windows.net/", nil)
+    if err != nil {
+        log.Fatalf("Failed to create blob client: %v", err)
+    }
+
+    return &BlobStorage{
+        client:        client,
+        containerName: "mycontainer",
+    }
+}
+
 func (s *BlobStorage) Upload(file io.Reader, filename string) error {
-    // ใช้ Azure SDK upload ไป blob storage
     containerClient := s.client.NewContainerClient(s.containerName)
     blobClient := containerClient.NewBlockBlobClient(filename)
 
